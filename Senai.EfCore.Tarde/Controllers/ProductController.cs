7c776007dd51aab501ba8cc2f663604d82a8f035
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-09-14 21:48:07 -0300

    Aula 8]

--- Senai.EfCore.Tarde/Controllers/ProductController.cs
+++ Senai.EfCore.Tarde/Controllers/ProductController.cs
@@ -12,48 +12,108 @@ namespace Senai.EfCore.Tarde.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
-    {
+    public class ProductController : ControllerBase {
         private readonly IProductRepository _productRepository;
 
-        public ProductController()
-        {
+        public ProductController() {
             _productRepository = new ProductRepository();
         }
-
         [HttpGet]
-        public List<Product> Get()
-        {
-            return _productRepository.List();
+        public IActionResult Get() {
+            try {
+                // List the products
+                var products = _productRepository.List();
+
+
+                // Check if there is a registered product
+                if (products.Count == 0)
+                    return NoContent();
+
+                return Ok(new {
+                    totalCount = products.Count,
+                    data = products
+                });
+            }
+            catch (Exception ex) {
+                // TODO: Register error message in the logError domain
+                return BadRequest(new {
+                    statusCode = 400,
+                    error = "Ocorreu um erro no endpoint Get/products, envie um e-mail para email@email.com informando"
+                });
+            }
         }
 
         // GET api/<RacaController>/5
         [HttpGet("{id}")]
-        public Product Get(Guid id)
-        {
-            return _productRepository.SearchById(id);
-        }
 
+        public IActionResult Get(Guid id) {
+            try {
+                //Busco o produto pelo Id
+                Product product = _productRepository.SearchById(id);
+
+                //Verifico se o produto foi encontrado
+                //Caso não exista retorno NotFounf
+                if (product == null)
+                    return NotFound();
+
+                //Caso exista retorno Ok e os dados do produto
+                return Ok(product);
+            }
+            catch (Exception ex) {
+                //Caso ocorra algum erro retorno BadRequest e a mensagem da exception
+                return BadRequest(ex.Message);
+            }
+        }
         // POST api/<AlunoController>
         [HttpPost]
-        public void Post(Product product)
-        {
-            _productRepository.Add(product);
+        public IActionResult Post(Product product) {
+            try {
+                //Add a new product
+                _productRepository.Add(product);
+
+                // Returns Ok
+                return Ok(product);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<AlunoController>/5
         [HttpPut("{id}")]
-        public void Put(Guid id, Product product)
-        {
-            product.Id = id;
-            _productRepository.Edit(product);
+        public IActionResult Put(Guid id, Product product) {
+            try {
+                // Edit the product
+                _productRepository.Edit(product);
+
+                // Returns Ok with the product data
+                return Ok(product);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<AlunoController>/5
         [HttpDelete("{id}")]
-        public void Delete(Guid id)
-        {
-            _productRepository.Remove(id);
+        public IActionResult Delete(Guid id) {
+            try {
+                // Search the product by Id
+                var product = _productRepository.SearchById(id);
+
+                //Checks whether product exists
+                //If not, return NotFound
+                if (product == null)
+                    return NotFound();
+
+                //If it exists remove the product
+                _productRepository.Remove(id);
+                //Return
+                return Ok(id);
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
--- Senai.EfCore.Tarde/Repositories/ProductRepository.cs
+++ Senai.EfCore.Tarde/Repositories/ProductRepository.cs
@@ -5,14 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Senai.EfCore.Tarde.Repositories
-{
+namespace Senai.EfCore.Tarde.Repositories {
     public class ProductRepository : IProductRepository {
         private readonly RequestContext _ctx;
         public ProductRepository() {
             _ctx = new RequestContext();
         }
-
         #region Reading
         // List all registered products
         public List<Product> List() {
@@ -23,8 +21,6 @@ namespace Senai.EfCore.Tarde.Repositories
                 throw new Exception(ex.Message);
             }
         }
-
-
         // Search for a product by Id
         public Product SearchById(Guid id) {
             try {
@@ -36,10 +32,8 @@ namespace Senai.EfCore.Tarde.Repositories
                 throw new Exception(ex.Message);
             }
         }
-
-
         // Search products by name
-        public List<Product> BuscarPorNome(string name) {
+        public List<Product> SearchByName(string name) {
             try {
                 return _ctx.Products.Where(c => c.Name.Contains(name)).ToList();
             }
@@ -48,11 +42,10 @@ namespace Senai.EfCore.Tarde.Repositories
             }
         }
         #endregion
-
         #region Recording
 
         // Edit a product
-        public void Editar(Product product) {
+        public void Edit(Product product) {
             try {
                 // Search product by id
                 Product productTemp = SearchById(product.Id);
@@ -76,17 +69,10 @@ namespace Senai.EfCore.Tarde.Repositories
             }
         }
 
-
         // Add a new product
-        public void Adicionar(Product product) {
+        public void Add(Product product) {
             try {
-                //Add product object to context dbset
                 _ctx.Products.Add(product);
-
-                //_ctx.Set<Product>().Add(product);
-                //_ctx.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Added;
-
-                //Save changes in context
                 _ctx.SaveChanges();
             }
             catch (Exception ex) {
@@ -114,19 +100,6 @@ namespace Senai.EfCore.Tarde.Repositories
                 throw new Exception(ex.Message);
             }
         }
-
-
-    public List<Product> SearchByName(string name) {
-        throw new NotImplementedException();
+        #endregion
     }
-
-    public void Add(Product product) {
-        throw new NotImplementedException();
-    }
-
-    public void Edit(Product product) {
-        throw new NotImplementedException();
-    }
-    #endregion
-}
 }
